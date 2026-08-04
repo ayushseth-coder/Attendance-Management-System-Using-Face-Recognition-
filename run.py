7b19965aa5ea201b279f06d642f2b047b0cb69e8
ayushseth-coder <ayushseth042@gmail.com>
2026-08-04 16:04:54 +0530

    Lazy load DeepFace and limit TensorFlow memory to fit 512MB RAM

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
+import os
+# --- RAM OPTIMIZATION FOR CLOUD HOSTING (512MB RAM LIMIT) ---
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['TF_NUM_INTRAOP_THREADS'] = '1'
+os.environ['TF_NUM_INTEROP_THREADS'] = '1'
+os.environ['MALLOC_TRIM_THRESHOLD_'] = '65536'
+# -------------------------------------------------------------
+
 from main import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-
